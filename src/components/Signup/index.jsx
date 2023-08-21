522e41c904e1ea697787b25aa49c9173ef75ccf9
Lê Chí Luân (Brian Lee) <chiluan6601@gmail.com>
2023-08-21 19:35:45 +0700

    Update server

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
     const handleSubmit = async (e)=> {
         e.preventDefault();
         try {
-            const url = "http://15.235.203.180:5050/api/users"
+            const url = "https://15.235.203.180:5050/api/users"
             const {data:res} = await axios.post(url,data);
             navigate("/signin");
             console.log(res.message);
