d61feaf93937e36ec2e8024c4cf7623d26e9837a
Lê Chí Luân (Brian Lee) <chiluan6601@gmail.com>
2023-08-21 20:45:10 +0700

    Update client

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
     const handleSubmit = async (e)=> {
         e.preventDefault();
         try {
-            const url = "https://15.235.203.180:443/api/users"
+            const url = "http://15.235.202.83:5050/api/users"
             const {data:res} = await axios.post(url,data);
             navigate("/signin");
             console.log(res.message);
